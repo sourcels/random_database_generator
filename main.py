bc1d169983a6bdb5a485d8a69630ec62d1dbc2d8
Dmytro Shiper <75039026+sourcels@users.noreply.github.com>
2022-07-15 15:08:34 +0200

    Updated main.py
    main.py no more generator, now it is introducing

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
-import json
-from random import randint
+from generator import generate_json, create_users_list, generate_password, generate_user, return_schema
 
-# data source
-results = list()
-names = ["Henry", "Alice", "John", "Marzipan", "Dima", "Dan", "Anna", "Kate", "David", "Felix", "George", "Rachel", "Max", "Daniel"]
-s_names = ["Blackwooth", "Northest", "Richter", "Shiper", "Herrington", "Teltow", "Askrop", "Biss", "Koval", "Kustk", "Xian", "Stein", "Minch", "Brown"]
-genders = ["Man", "Woman", "Other"]
-jobs = ["Farmer", "Program Engineer", "Program Distributor", "CEO", "Data Scientist", "UIX Designer", "Designer", "Backend Developer", "Frontend Developer", "Fullstack Developer", "Devops", "Tester", "Publisher"]
+generate_json(indent=4, user_amount=20, path="path/to/file.json")
 
-# user amount
-users_amount = randint(1, 100)
+test_list = create_users_list(user_amount=20)
 
-# data generation
-for i in range(users_amount):
-    item = dict()
-    name = randint(0, len(names)-1)
-    s_name = randint(0, len(s_names)-1)
-    gender = randint(0, len(genders)-1)
-    job = randint(0, len(jobs)-1)
+test_string = generate_password(length=15)
 
-    item['user'] = f"{names[name]} {s_names[s_name]}"
-    item['age'] = randint(18, 90)
-    item['sex'] = genders[gender]
-    item['job'] = jobs[job]
-    item['login'] = f"{names[name][:4]}{s_names[s_name][2:]}_{randint(10000, 99999)}"
-    item['password'] = f"{randint(100000, 999999)}_{randint(222222, 888888)}"
+test_dict = generate_user()
 
-    results.append(item)
+test_dict1 = return_schema()
 
-# prettify data
-results = sorted(results, key=lambda d: d['user'])
-jsonString = json.dumps(results, indent=4)
-
-# writing file
-jsonFile = open("data.json", "w")
-jsonFile.write(jsonString)
-jsonFile.close()
